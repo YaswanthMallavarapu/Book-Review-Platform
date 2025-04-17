e25177314d12d13ff5b658de81a561377d05faa7
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2025-04-17 16:36:59 +0530

    first commit

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://book-review-platform-65fn-yaswanthmallavarapus-projects.vercel.app/api/";
+  "https://book-review-platform-65fn-yaswanthmallavarapus-projects.vercel.app/api";
